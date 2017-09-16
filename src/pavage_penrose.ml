c3b4607a8a1c282d0997e52be23c05968dcc4f9a
Clement Legrand <clegrand@caraja.inrialpes.fr>
2017-09-16 10:34:12 +0200

    using shorter function names
    Maybe we should also change int_of_float to iof by I don't know if it's
    really necessary

--- src/pavage_penrose.ml
+++ src/pavage_penrose.ml
@@ -8,20 +8,23 @@ let taille = 500.;;
 type triangle = Obtuse | Acute ;;
 let move a b = moveto (int_of_float a) (int_of_float b);;
 let line a b = lineto (int_of_float a) (int_of_float b);;
-let int_of_float_array tab =
+let iof_array tab =
   let int_tab = Array.create (Array.length tab) (0,0) in
   for i= 0 to Array.length tab do
     let (x,y) = tab.(i) in int_tab.(i) <- (int_of_float x, int_of_float y) 
   done; int_tab;;
   
-									   
+let wait t =
+  for i = 0 to t do
+    print_string " "
+  done;;
   
 let draw points triangle =
   (if triangle = Obtuse then set_color red
   else set_color blue);
   let (p1x,p1y) = points.(0) and (p2x,p2y) = points.(1) and (p3x,p3y) = points.(2) in
-  move p1x p1y; line p2x p2y;
-  line p3x p3y; line p1x p1y; fill_poly (int_of_float_array points);;
+  move p1x p1y; line p2x p2y;  
+  line p3x p3y; line p1x p1y (* ; fill_poly (iof_array points) *);;
   
 
 let a = taille *. (sqrt (golden_ratio*.golden_ratio -. 0.25));;
