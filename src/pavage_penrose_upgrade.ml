3ac14264e9171532778a7bbb9eecbe1f3332cd8b
Clement Legrand <clegrand@caraja.inrialpes.fr>
2017-09-17 15:06:26 +0200

    a few upgrades
    divide is now drawing the lines after filling the triangles.
    divide2 is showing the progress of the tilling.
    TODO: the lines are drawn before the filling in divide2, so we can't
    really see them after...
    Add short break between two generations.

--- src/pavage_penrose_upgrade.ml
+++ src/pavage_penrose_upgrade.ml
@@ -13,33 +13,55 @@ let homothety tab factor = Array.map (fun (x,y) -> (x*.factor,y*.factor)) tab;;
  
 let draw points triangle =
   set_color black;
-  move points.(0); line points.(1);  
-  line points.(2); line points.(0);
   (if triangle = Obtuse then set_color red
   else set_color blue);
-  fill_poly (iof_array points);; 
+  fill_poly (iof_array points);;
   
-
 let a = taille *. (sqrt (golden_ratio*.golden_ratio -. 0.25));;
-
   
-let rec divide generation points triangle (* init_generation *)=
-  if generation = 0 then
-    draw points triangle (* init_generation *)
+let rec divide generation points triangle =
+  if generation = 0 then draw points triangle
   else begin
       let (p1x,p1y) = points.(0) and (p2x,p2y) = points.(1) and (p3x,p3y) = points.(2) in
       if triangle = Obtuse then
 	(let newpoint = (p2x +.(p3x-.p2x)/.(1.+.golden_ratio), p2y +.(p3y-.p2y)/.(1.+.golden_ratio)) in
-	 divide (generation -1) [|newpoint; points.(0); points.(1)|] Obtuse (* init_generation *);
-	 divide (generation -1) [|points.(2);points.(0); newpoint|] Acute (* init_generation*) ) 
+	 divide (generation -1) [|newpoint; points.(0); points.(1)|] Obtuse;
+	 divide (generation -1) [|points.(2);points.(0); newpoint|] Acute;
+	 set_color black; move points.(0); line newpoint)
       else 
 	(let  newpoint1 = (p1x +.(p2x-.p1x)/.(1.+.golden_ratio), p1y +.(p2y-.p1y)/.(1.+.golden_ratio)) in
-	let newpoint2 = (p3x +.(p1x-.p3x)/.(1.+.golden_ratio), p3y +.(p1y-.p3y)/.(1.+.golden_ratio)) in
-	divide (generation -1) [|newpoint1; points.(0); newpoint2 |] Obtuse (* init_generation *); 
-	divide (generation -1) [|points.(1); points.(2);  newpoint2|] Acute (* init_generation *);
-	divide (generation -1) [|points.(1); newpoint2; newpoint1|] Acute (* init_generation *))
+	 let newpoint2 = (p3x +.(p1x-.p3x)/.(1.+.golden_ratio), p3y +.(p1y-.p3y)/.(1.+.golden_ratio)) in
+	 divide (generation -1) [|newpoint1; newpoint2; points.(0)|] Obtuse; 	     divide (generation -1) [|points.(1); points.(2); newpoint2|] Acute;
+	 divide (generation -1) [|points.(1); newpoint2; newpoint1|] Acute;
+	 set_color black; move newpoint1;
+	 line newpoint2; line points.(1))
     end;;
 
- (* divide 3 [|(taille*.golden_ratio/.2., taille*.sqrt(1.-.golden_ratio*.golden_ratio /. 4.)); (taille*. golden_ratio,0.); (0.,0.)|] Obtuse ;; *)
+(* divide 5 [|(taille*. sqrt (golden_ratio *.golden_ratio -. 0.25), taille*.0.5); (0.,0.); (0., taille)|] Acute;; *)
 
-divide 5 [|(taille*. sqrt (golden_ratio *.golden_ratio -. 0.25), taille*.0.5); (0.,0.); (0., taille)|] Acute;;
+type triangle_liste = ((float * float) array * triangle) list;;
+
+let divide2 generation points triangle =
+  let rec aux u v =
+    match u with
+    | [] -> v
+    | u1::us -> (let ([|(p1x,p1y);(p2x,p2y);(p3x,p3y)|],triangle_type) = u1 in
+		 if triangle_type = Obtuse then
+		   (let newpoint = (p2x +.(p3x-.p2x)/.(1.+.golden_ratio), p2y +.(p3y-.p2y)/.(1.+.golden_ratio)) in
+		    set_color black; move (p1x,p1y); line newpoint;
+		     aux us (([|newpoint; (p1x,p1y); (p2x,p2y)|], Obtuse)::([|(p3x,p3y); (p1x,p1y); newpoint|], Acute)::v))
+		 else 
+		   (let  newpoint1 = (p1x +.(p2x-.p1x)/.(1.+.golden_ratio), p1y +.(p2y-.p1y)/.(1.+.golden_ratio)) in
+		    let newpoint2 = (p3x +.(p1x-.p3x)/.(1.+.golden_ratio), p3y +.(p1y-.p3y)/.(1.+.golden_ratio)) in
+		    set_color black; move newpoint1;
+		    line newpoint2; line (p2x,p2y);
+		    aux us (([|newpoint1; newpoint2; (p1x,p1y)|], Obtuse)::([|(p2x,p2y); (p3x,p3y); newpoint2|], Acute)::([|(p2x,p2y); newpoint2; newpoint1|], Acute)::v)))
+  in
+  let rec aux2 generation u =
+    if generation = 0 then List.map (fun (tab, triangle_type) -> draw tab triangle_type) u
+    else let v = aux u [] in aux2 (generation - 1) v
+  in
+  aux2 generation [(points, triangle)];();;
+
+divide 8 [|(taille*. sqrt (golden_ratio *.golden_ratio -. 0.25), taille*.0.5); (0.,0.); (0., taille)|] Acute;;
+ 
