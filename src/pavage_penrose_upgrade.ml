2e1e3a0dec949af737422875acac18d206b6a7e2
Clement Legrand <clegrand@caraja.inrialpes.fr>
2017-09-22 09:02:18 +0200

    In bounds

--- src/pavage_penrose_upgrade.ml
+++ src/pavage_penrose_upgrade.ml
@@ -18,29 +18,40 @@ let homothety tab factor = Array.map (fun (x,y) -> (x*.factor,y*.factor)) tab;;
 let draw points triangle =
   (if triangle = Obtuse then set_color red
   else set_color blue);
-  fill_poly (iof_array points);;
-  
-let a = taille *. (sqrt (golden_ratio*.golden_ratio -. 0.25));;
+  fill_poly (iof_array (homothety points 1.));;
+
+let in_bounds pts_triangle =
+  let [|(p1x,p1y); (p2x,p2y); (p3x,p3y)|] = pts_triangle in
+  (p1x >= 0. && p1x <= width && p1y >= 0. && p1y <= height &&
+     p2x >= 0. && p2x <= width && p2y >= 0. && p2y <= height &&
+     p3x >= 0. && p3x <= width && p3y >= 0. && p3y <= height);;
+    
+let a = size *. (sqrt (golden_ratio*.golden_ratio -. 0.25));;
   
 let rec divide generation points triangle =
-  if generation = 0 then draw points triangle
+  if generation = 0 then () (* draw points triangle *)
   else begin
       let (p1x,p1y) = points.(0) and (p2x,p2y) = points.(1) and (p3x,p3y) = points.(2) in
       if triangle = Obtuse then
 	(let newpoint = (p2x +.(p3x-.p2x)/.(1.+.golden_ratio), p2y +.(p3y-.p2y)/.(1.+.golden_ratio)) in
 	 divide (generation -1) [|newpoint; points.(0); points.(1)|] Obtuse;
 	 divide (generation -1) [|points.(2);points.(0); newpoint|] Acute;
-	 set_color black; move points.(0); line newpoint)
-      else 
+	 set_color black;
+	 move points.(0);
+	 line newpoint)
+      else
 	(let  newpoint1 = (p1x +.(p2x-.p1x)/.(1.+.golden_ratio), p1y +.(p2y-.p1y)/.(1.+.golden_ratio)) in
 	 let newpoint2 = (p3x +.(p1x-.p3x)/.(1.+.golden_ratio), p3y +.(p1y-.p3y)/.(1.+.golden_ratio)) in
-	 divide (generation -1) [|newpoint1; newpoint2; points.(0)|] Obtuse; 	     divide (generation -1) [|points.(1); points.(2); newpoint2|] Acute;
+	 divide (generation -1) [|newpoint1; newpoint2; points.(0)|] Obtuse;
+	 divide (generation -1) [|points.(1); points.(2); newpoint2|] Acute;
 	 divide (generation -1) [|points.(1); newpoint2; newpoint1|] Acute;
-	 set_color black; move newpoint1;
-	 line newpoint2; line points.(1))
+	 set_color black;
+	 move newpoint1;
+	 line newpoint2;
+	 line points.(1))
     end;;
 
-(* divide 5 [|(taille*. sqrt (golden_ratio *.golden_ratio -. 0.25), taille*.0.5); (0.,0.); (0., taille)|] Acute;; *)
+(* divide 5 [|(size*. sqrt (golden_ratio *.golden_ratio -. 0.25), size*.0.5); (0.,0.); (0., size)|] Acute;; *)
 
 type triangle_liste = ((float * float) array * triangle) list;;
 
